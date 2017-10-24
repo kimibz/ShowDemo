14b1683e095292ca23a47b4b3eb235d48490ee74
Administrator <Administrator@192.168.196.1>
2017-10-24 08:52:04 +0800

    Team

---
+++ SpringWeb2/src/main/java/com/xigua/TimeJob/TestTimer.java
@@ -0,0 +1,20 @@
+package com.xigua.TimeJob;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestTimer {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(TestTimer.class);
+    
+    /**  
+     * 业务逻辑定时处理  
+     */  
+    public void execute() {  
+        // 执行业务逻辑  
+        // ........  
+        //LOG.info("定时任务.......");//为什么打不出LOG
+        //System.out.println("定时任务");
+    } 
+
+}
